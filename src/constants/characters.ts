e66d19f16f5caa200199fbfe101e4afee3a019ed
hiimsewek <102280@stud.uz.zgora.pl>
2023-11-18 16:13:28 +0100

    Remove symbols causing regexp errors

--- src/constants/characters.ts
+++ src/constants/characters.ts
@@ -1,4 +1,4 @@
 export const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
 export const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const NUMBERS = "1234567890";
-export const SYMBOLS = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+export const SYMBOLS = "`~!@#$%^&*()_=+{}|;:'\",<.>/?";
