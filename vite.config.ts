5743071562886e0634a79d55612991560c98c8e4
hiimsewek <102280@stud.uz.zgora.pl>
2023-11-19 18:47:40 +0100

    Set up service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      strategies: "generateSW",
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+    }),
+  ],
   resolve: {
     alias: {
       assets: "/src/assets",
